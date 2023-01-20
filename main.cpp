6fc3d7b96a70271c1a5af3c2e0f27e3c503afc29
Dariusz Strojny <dstrojny99@gmail.com>
2023-01-20 02:09:29 +0100

    Komentarze, sprawozdanie

--- main.cpp
+++ main.cpp
@@ -12,35 +12,46 @@ std::vector<int> gnome_sort(std::vector<int> array);
 // algorytm sortowania kubełkowego
 std::vector<int> bucket_sort(std::vector<int> array, std::pair<int, int> range);
 
+// funkcja testująca poprawność wykonania algorytmów sortowania
 void test_sort(const std::vector<int>& arr)
 {
+    // dla każdego elementu tablicy zaczynając od 1
     for (size_t i = 1; i < arr.size(); i++)
-        if (arr[i] < arr[i - 1]) throw "Przeprowadzono bledne sortowanie!";
+        // wyrzuć błąd, jeżeli poprzedni element jest większy
+        if (arr[i - 1] > arr[i]) throw std::string("Przeprowadzono bledne sortowanie!");
 }
 
 int main()
 {
+    // inicjalizacja funkcji pomocniczych
     pro::init();
 
-    std::pair<int, int> range = { -10, 10 };
+    // stworzenie wybranego zakresu początkowego dla wszystkich testów
+    std::pair<int, int> range = { 0, 1 };
+
+    // wykonanie trzech zestawów testów
     for (int i = 1; i <= 3; i++)
     {
 
-        auto wyniki = testy(6, 2000, range, 2);
-
+        // przechwytywanie wszystkich błędów typu std::string
         try
         {
+            // wpisanie wyników testów do tabeli
+            auto wyniki = testy(8, 1000, range, 2);
+
+            // zapisanie tabeli do pliku odpowiadającego zestawowi testów
             std::string filename = std::string("test/Testy ") + std::to_string(i) + ".txt";
             pro::zapisz_ciag_2d_do_pliku(filename.c_str(), wyniki);
             std::cout << "Wyniki zapisane w pliku " << filename << "\n";
         }
         catch (std::string& e)
         {
+            // wypisanie przechwyconego błędu
             std::cout << "Error: " << e << "\n";
         }
 
-        range.first *= 100;
-        range.second *= 100;
+        range.first *= 500;
+        range.second *= 500;
     }
 
 
@@ -51,54 +62,71 @@ int main()
 // funkcja wywołująca testy czasów na obu algorytmach
 std::vector<std::vector<double>> testy(int ilosc_testow, int start_len, std::pair<int, int> range_, int multiplier)
 {
+    // inicjalizacja struktur czasu
     std::chrono::high_resolution_clock::time_point start, stop;
     std::chrono::duration<double> timediff;
 
+    // definicja tabeli wyników czasów trwania i obliczeń
     std::vector<std::vector<double>> wyniki;
-
     std::vector<int> wynik_obliczen;
 
     int arr_len;
 
+    // dla podanej ilości testów
     for (int nr_testu = 0; nr_testu < ilosc_testow; nr_testu++)
     {
+        // obliczanie mnożnika dla aktualnego numeru testu
         int mp_pow = std::pow(multiplier, nr_testu);
+
+        // obliczenie zakresu losowanych liczb
         std::pair<int, int> range = { range_.first * mp_pow, range_.second * mp_pow };
 
+        // obliczanie długości generowanego ciągu
         arr_len = start_len * mp_pow;
 
+        // definicja tablicy przechowującej wyniki pojedyńczego testu
         std::vector<double> zebrane_dane;
 
+        // wpisanie ilości danych do wyników
         zebrane_dane.push_back(arr_len);
 
         std::cout << "Test nr: " << nr_testu + 1 << "\n";
         std::cout << "Generowanie tablicy o rozmiarze " << arr_len << " i zakresie danych " << range.first << " do " << range.second << "\n";
 
+        // generowanie ciągu o zadanych parametrach
         auto data = pro::generuj_losowy_ciag(range.first, range.second, arr_len);
 
         std::cout << "Tablica wejsciowa: ";
         pro::opisz_ciag(data);
 
+        // wykonanie testu dla pierwszego algorytmu z pomiarem czasu
         std::cout << "Start A:\n";
         start = std::chrono::high_resolution_clock::now();
         wynik_obliczen = gnome_sort(data);
         stop = std::chrono::high_resolution_clock::now();
+        // sprawdzenie poprawności wyników
         test_sort(wynik_obliczen);
 
+        // obliczenie czasu trwania algorytmu
         timediff = stop - start;
 
+        // wpisanie czasu do wyników
         zebrane_dane.push_back(timediff.count());
         std::cout << "Czas wykonania algorytmu A: " << timediff.count() << "\n";
         std::cout << "Ilosc wynikow: " << wynik_obliczen.size() << "\n";
 
+        // wykonanie testu dla drugiego algorytmu z pomiarem czasu
         std::cout << "Start B:\n";
         start = std::chrono::high_resolution_clock::now();
         wynik_obliczen = bucket_sort(data, range);
         stop = std::chrono::high_resolution_clock::now();
+        // sprawdzenie poprawności wyników
         test_sort(wynik_obliczen);
 
+        // obliczenie czasu trwania algorytmu
         timediff = stop - start;
 
+        // wpisanie czasu do wyników
         zebrane_dane.push_back(timediff.count());
         std::cout << "Czas wykonania algorytmu B: " << timediff.count() << "\n";
         std::cout << "Ilosc wynikow: " << wynik_obliczen.size() << "\n";
@@ -107,7 +135,7 @@ std::vector<std::vector<double>> testy(int ilosc_testow, int start_len, std::pai
         pro::opisz_ciag(wynik_obliczen);
         std::cout << "\n";
 
-
+        // wpisanie wyników do zwracanej tabeli
         wyniki.push_back(zebrane_dane);
 
     }
@@ -171,7 +199,7 @@ std::vector<int> bucket_sort(std::vector<int> array, std::pair<int, int> range)
     // dla każdego elementu w tablicy
     for (const int& el : array)
         // zwiększenie wartość licznika o indeksie tego elementu
-        buckets[el - range.first]++
+        buckets[el - range.first]++;
 
     // definicja iteratora "i" przechodzącego przez tablice liczników
     int i = 0;
